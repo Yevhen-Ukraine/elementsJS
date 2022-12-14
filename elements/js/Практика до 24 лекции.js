389b48ea52605be1b43cd811c048497215ba476f
Yevhen-Ukrain <tishenko130180@gmail.com>
2022-12-14 23:46:29 +0200

    1

--- elements/js/Практика до 24 лекции.js
+++ elements/js/Практика до 24 лекции.js
@@ -625,3 +625,18 @@ function getMathResult(num, times) {
 
 getMathResult(10, 5);
 
+function calculateVolumeAndArea(length) {
+    if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+        return "При вычислении произошла ошибка";
+    }
+    
+    let volume = 0,
+        area = 0;
+        
+    volume = length * length * length;
+    area = 6 * (length * length);
+    
+    return `Объем куба: ${area}, площадь всей поверхности: ${area}`;
+}
+
+calculateVolumeAndArea(5);
