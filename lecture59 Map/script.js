430831d19502208f51b82cf1bafc8b65b6eae9bd
Yevhen-Ukraine <tishenko130180@gmail.com>
2023-03-30 16:33:23 +0300

    1

---
+++ lecture59 Map/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+cont user = {
+    name: 'Alex';
+    surname: 'Smith';
+    birthdey: '20/04/1993',
+    swowMyPablicData: function () {
+        console.log(`${this.name} ${this.surname}`);
+    }
+}
+console.log(user);
+
+/* Map называються спецефические структуры данных которрые очень похожи на обьект, т.е. может использоваться */
