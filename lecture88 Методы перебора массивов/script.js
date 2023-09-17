c3474f0909e124456e9fde4094f78ec1862ee4d6
Yevhen-Ukraine <tishenko130180@gmail.com>
2023-09-17 16:49:29 +0200

    88

--- lecture88 Методы перебора массивов/script.js
+++ lecture88 Методы перебора массивов/script.js
@@ -39,3 +39,41 @@ const some = [4, 'jhf', 'uytuyuy'];
 
 console.log(some.every(item => typeof(item) === 'number'));//Все элементы массива должны соответствовать условию. Результат false
 
+// reduce (Служит для схлопывания массива в одно целое {Используется в основном для числовых данных})
+
+// const arr = [4, 5, 1, 3, 2, 6];
+// // 0    4
+// // 4    5
+// // 9    1
+// // 10   3
+// // и т.д.
+
+// const res = arr.reduce((sum, current) => sum + current, 3);
+// Метод reduce может принимать еще один аргумент, им можно задавать начальное значение. В таком случае при первом цикле sum будет равняться не 0, а 3. Результат в таком случае будет равен 24. 
+
+// console.log(res); 
+
+// Результат 21
+
+/* Вариант с строками */
+
+const arr = ['apple', 'pear', 'plum'];
+
+
+const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+console.log(res);
+
+
+// Пример использования, допустим имее данные с сервера или просто обьект
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    kat: 'animal'
+};
+
+const newArr = Object.entries(obj).filter(item => item[1] === 'persone').map(item => item[0]);
+
+console.log(newArr);
